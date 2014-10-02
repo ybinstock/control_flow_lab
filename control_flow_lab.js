d579b8fd4e6afa3ed5faaa8d60f9d5c2d9f98a89
ybinstock <ybinstock@gmail.com>
2014-10-01 18:49:00 -0700

    hw3

---
+++ control_flow_lab.js
@@ -0,0 +1,55 @@
+
+
+
+// Question 1
+
+// var foods = ["apples", "bannanas", "pears", "radishes"];
+// if (foods.length === 0){
+// 	console.log("You have no food!");
+// }
+// else if (foods.length===1) {
+// 	console.log("You have one left!");}
+// else {
+// 	console.log("You have tons of food");
+// }
+
+
+// var foods = ["apples", "bannanas", "pears", "radishes"];
+// if (foods.length===0){
+// 	console.log("DA FUCKKK?")
+// 	console.log("You have no food!");
+// }
+// else if (foods.length===1) {
+// 	console.log("You have one left!");
+// }
+// else {
+// 	console.log("You have tons of food");
+// }
+
+
+// Question 2
+var bottles = 99;
+while ( bottles > 1)
+
+{
+	console.log (bottles+" "+"bottles of beer on the wall" + " "+ bottles+
+		"bottles of beer"+"knock one down, pass it around"+" "+
+		bottles+"bottles of beer on the wall");
+	bottles--;
+
+}
+
+	console.log ("Last beer left. For me!!!!");
+
+
+
+
+
+// Question 3
+
+// var people = ["maia", "wally", "victor", "mike"];
+
+// for (i =0; i<people.length; i++ ) {
+// 	console.log("Hello,"+people[i]+"!");
+// }
+
